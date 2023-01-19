3f1a2c1a6d21c52d931db9930b16857bfbacca5b
tonymarsh <tonymarsh0331@gmail.com>
2023-01-19 16:15:54 +0800

    hashMap拉链法解决冲突的实现

---
+++ src/HashMap/MyHashMap1.java
@@ -0,0 +1,115 @@
+package HashMap;
+
+import java.util.Map;
+
+/**
+ * 使用拉链思路处理哈希冲突的 HashMap
+ * 主要的部分就是用myHash(key) 计算对应的Slot，剩下的就是用Slot的API来进行CRUD了
+ * 难点在于hash与rehash的设计，这方面有很多优化的空间，但这里仅仅是一个简单的实现用于阐释思路，所以不多展开了
+ */
+public class MyHashMap1<K, V> {
+    private static final int INIT_CAP = 16; // 默认初始容量
+    private static final float LOAD_FACTOR = 0.75f; // 负载因子
+    private int size;
+    private Slot<K, V>[] table; // 本哈希表本质是一个单链表数组
+
+    public MyHashMap1() {
+        this(INIT_CAP);
+    }
+
+    public MyHashMap1(int initCapacity) {
+        size = 0;
+        table = (Slot<K, V>[]) new Slot[initCapacity];
+        // 初始化每个槽位（虚拟头尾指针
+        for (int i = 0; i < initCapacity; i++) {
+            table[i] = new Slot<>();
+        }
+    }
+
+    //region CRUD
+
+    /*
+     */
+
+    /**
+     * 添加元素，如果key已经存在，则覆盖旧值
+     */
+    public V put(K key, V val) {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null");
+        }
+        if (size >= table.length * LOAD_FACTOR) {
+            resize(table.length * 2);
+        }
+        Slot<K, V> slot = table[myHash(key)];
+        if (!slot.containKey(key)) {
+            size++;
+        }
+        return slot.put(key, val);
+    }
+
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null");
+        }
+        Slot<K, V> slot = table[myHash(key)];
+        if (slot.containKey(key)) {
+            size--;
+            return slot.remove(key);
+        }
+        return null;
+    }
+
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null");
+        }
+        Slot<K, V> slot = table[myHash(key)];
+        return slot.get(key);
+    }
+
+
+    //endregion
+
+    //region Utils
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean containKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null");
+        }
+        Slot<K, V> slot = table[myHash(key)];
+        return slot.containKey(key);
+    }
+
+    /**
+     * 将任意key映射到HashTable的Index,也有很多种实现方式和优化方案，这里不展开了
+     */
+    private int myHash(K key) {
+//        取绝对值后与哈希表长度取模，保证哈希值在 [0, table.length) 范围内
+        return (key.hashCode() & 0x7fffffff) % table.length;
+    }
+
+    /**
+     * 重点在于rehash的部分，这方面有很多优化的空间可以挖掘 这里就不深入了
+     */
+    private void resize(int newCap) {
+        MyHashMap1<K, V> newMap = new MyHashMap1<>(newCap);
+        for (Slot<K, V> slot : table) {
+            for (Map.Entry<K, V> entry : slot.entries()) {
+                K key = entry.getKey();
+                V val = entry.getValue();
+                newMap.put(key, val);
+            }
+        }
+        this.table = newMap.table;
+    }
+
+    //endregion
+}
---
+++ src/HashMap/Slot.java
@@ -0,0 +1,161 @@
+package HashMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * HashMap拉链法实现中的槽位，(哈希桶)，本质是一个单链表
+ *
+ * @param <K>
+ * @param <V>
+ */
+public class Slot<K, V> {
+    // 头尾节点占位符
+    private final Node<K, V> head, tail;
+    private int size;
+    /**
+     * 构造函数，初始化头尾节点
+     */
+    public Slot() {
+        this.head = new Node<>(null, null);
+        this.tail = new Node<>(null, null);
+        head.next = tail;
+        this.size = 0;
+    }
+
+    //region 增
+    public V put(K key, V val) {
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null");
+        }
+        Node<K, V> p = getNode(key);
+        // 如果key已经存在，更新val
+        if (p != null) {
+            V oldValue = p.val;
+            p.val = val;
+            return oldValue;
+        }
+        // 如果key不存在，插入新节点
+        Node<K, V> newNode = new Node<>(key, val);
+        newNode.next = head.next; // 单链表，头插
+        head.next = newNode;
+        size++;
+        return null;
+    }
+
+    //region 删
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null");
+        }
+        Node<K, V> pre = head;
+        while (pre.next != tail) {
+            if (pre.next.key.equals(key)) {
+                V oldValue = pre.next.val;
+                pre.next = pre.next.next;
+                size--;
+                return oldValue;
+            }
+            pre = pre.next;
+        }
+        return null;
+    }
+
+    //endregion
+
+    //region 查
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null");
+        }
+        Node<K, V> p = getNode(key);
+        return p == null ? null : p.val;
+    }
+
+    //endregion
+
+    public boolean containKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null");
+        }
+        return getNode(key) != null;
+    }
+
+    /**
+     * 遍历slot,返回key对应的node
+     */
+    private Node<K, V> getNode(K key) {
+        for (Node<K, V> p = head.next; p != tail; p = p.next) {
+            if (p.key.equals(key)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    //endregion
+
+    //region 改
+
+    //endregion
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public List<K> keys() {
+        ArrayList<K> keyList = new ArrayList<>();
+        for (Node<K, V> p = head.next; p != tail; p = p.next) {
+            keyList.add(p.key);
+        }
+        return keyList;
+    }
+
+    public List<Map.Entry<K, V>> entries() {
+        ArrayList<Map.Entry<K, V>> entryList = new ArrayList<>();
+        for (Node<K, V> p = head.next; p != tail; p = p.next) {
+            entryList.add(p);
+        }
+        return entryList;
+    }
+
+    /**
+     * 链表节点
+     *
+     * @param <K>
+     * @param <V>
+     */
+    private static class Node<K, V> implements Map.Entry<K, V> {
+        K key;
+        V val;
+        Node<K, V> next;
+
+        Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return val;
+        }
+
+        public V setValue(V val) {
+            V oldValue = this.val;
+            this.val = val;
+            return oldValue;
+        }
+
+    }
+
+}
