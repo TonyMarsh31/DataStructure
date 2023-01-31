1651b82098b8db64129968e9f213ea0dfd5fdc44
tonymarsh <tonymarsh0331@gmail.com>
2023-01-31 18:51:38 +0800

    引入递归，使用递归重构一些迭代代码

---
+++ src/BST/Intro_recursion.java
@@ -0,0 +1,33 @@
+package BST;
+
+public class Intro_recursion {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // 遍历这个数组
+        for (int j : arr) {
+            System.out.print(j);
+        }
+
+        System.out.println();
+
+        // 递归遍历这个数组
+        printArr(arr, 0);
+
+        int targetValue = 5;
+        int index = searchItem(arr, 0, targetValue);
+    }
+
+    private static int searchItem(int[] arr, int i, int targetValue) {
+        if (i == arr.length) return -1; // base case (没找到)
+        if (arr[i] == targetValue) return i; // base case (找到了)
+        return searchItem(arr, i + 1, targetValue); // recursion
+    }
+
+    private static void printArr(int[] arr, int i) {
+        if (i == arr.length) return; // base case
+        System.out.print(arr[i]);  // action
+        printArr(arr, i + 1); // recursion
+        // trick: 交换上面两行代码的位置，则会倒序打印数组，因为此时的action是从最深处开始执行的
+    }
+}
