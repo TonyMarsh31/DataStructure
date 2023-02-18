4f4f1848c472e42f7ab936d308dfd4086ab85396
tonymarsh <tonymarsh0331@gmail.com>
2023-02-18 23:49:12 +0800

    循环队列

---
+++ src/队列和栈/循环队列/ArrayCircularQueue.java
@@ -0,0 +1,75 @@
+package 队列和栈.循环队列;
+
+public class ArrayCircularQueue<E> {
+    private int size;
+    private E[] data;
+    private final static int INIT_CAP = 2;
+
+    /**
+     * 注意fist是elementIndex， last是positionIndex,指向下一个可以插入的位置
+     * <p>也就是说第一个元素是data[first]，最后一个元素时data[last-1]</p>
+     */
+    private int first, last;
+
+    public ArrayCircularQueue(int initCap) {
+        size = 0;
+        data = (E[]) new Object[initCap];
+        first = last = 0;
+    }
+
+    public ArrayCircularQueue() {
+        this(INIT_CAP);
+    }
+
+    private void resize(int newCap) {
+        E[] temp = (E[]) new Object[newCap];
+        // copyData
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[(first + i) % data.length];
+        }
+        first = 0;
+        last = size;
+        data = temp;
+    }
+
+    public void enqueue(E e) {
+        if (size == data.length) resize(size * 2);
+        data[last] = e;
+        last++;
+        if (last == data.length) last = 0;
+        size++;
+    }
+
+    public E dequeue() {
+        if (isEmpty()) throw new IllegalArgumentException("Queue is empty");
+        E oldVal = data[first];
+        data[first] = null;
+        first++;
+        if (first == data.length) first = 0;
+        size--;
+        if (size == data.length / 4 && data.length / 2 != 0) resize(data.length / 2);
+        return oldVal;
+    }
+
+    public E peekFirst() {
+        if (isEmpty()) throw new IllegalArgumentException("Queue is empty");
+        return data[first];
+    }
+
+    public E peekLast() {
+        if (isEmpty()) throw new IllegalArgumentException("Queue is empty");
+        if (last == 0) return data[data.length - 1];
+        return data[last - 1];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
+
+
---
+++ src/队列和栈/循环队列/LinkedListCircularQueue.java
@@ -0,0 +1,51 @@
+package 队列和栈.循环队列;
+
+
+import java.util.LinkedList;
+
+/**
+ * 没有特别大的意义，本质就是受限的LinkedList
+ * <p> 需要明白的是,LinkedList本身就是Deque了（实现了Deque接口）</p>
+ * 循环队列这个词应该就是用于用数组实现队列，然后复用数组空间的
+ * 这里用LinkedList实现，就是用LinkedList的addLast和removeFirst方法
+ */
+public class LinkedListCircularQueue {
+    LinkedList<Integer> list;
+    int size;
+
+    public LinkedListCircularQueue(int initCap) {
+        list = new LinkedList<>();
+        size = initCap;
+    }
+
+    public boolean enQueue(int value) {
+        if (list.size() == size) return false;
+        list.addLast(value);
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (list.isEmpty()) return false;
+        list.removeFirst();
+        return true;
+    }
+
+    public int Front() {
+        if (list.isEmpty()) return -1;
+        return list.peekFirst();
+    }
+
+    public int Rear() {
+        if (list.isEmpty()) return -1;
+        return list.peekLast();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean ifFull() {
+        return list.size() == size;
+    }
+
+}
