acd014121e5c6675af147c09a38e8827f7810861
tonymarsh <tonymarsh0331@gmail.com>
2023-02-21 23:40:51 +0800

    :bug: 修复错误，min不能直接获得.重构大部分结构

--- src/队列和栈/单调队列/MonotonicQueue.java
+++ src/队列和栈/单调队列/MonotonicQueue.java
@@ -1,32 +1,60 @@
 package 队列和栈.单调队列;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class MonotonicQueue<T extends Comparable<T>> {
-    private final LinkedList<T> data = new LinkedList<>();
+    private final Deque<T> max; // 一个单调队列，第一个为max，接下来是次max ……
+    private final Deque<T> min; // 一个单调队列，第一个为min，接下来是次min ……
+    private final Deque<T> data; // 存放数据的队列
+
+    public MonotonicQueue() {
+        // min和max在有序度不高的情况下，size不会很大，所以用ArrayDeque
+        max = new ArrayDeque<>(10);
+        min = new ArrayDeque<>(10);
+        data = new LinkedList<>();
+    }
 
     public void push(T n) {
-        while (!data.isEmpty() && data.getLast().compareTo(n) < 0) {
-            data.removeLast();
+        while (!max.isEmpty() && max.getLast().compareTo(n) < 0) {
+            max.removeLast();
         }
+        max.addLast(n);
+
+        while (!min.isEmpty() && min.getLast().compareTo(n) > 0) {
+            min.removeLast();
+        }
+        min.addLast(n);
+
         data.addLast(n);
     }
 
-    public void pop(T n) {
-        if (!data.isEmpty() && data.getFirst().compareTo(n) == 0) {
-            data.removeFirst();
+    public T pop(T n) {
+        T t = data.removeFirst();
+        if (!max.isEmpty() && max.getFirst().compareTo(t) == 0) {
+            max.removeFirst();
         }
+        if (!min.isEmpty() && min.getFirst().compareTo(t) == 0) {
+            min.removeFirst();
+        }
+        return t;
     }
 
     public T max() {
-        return data.getFirst();
+        return max.getFirst();
     }
 
     public T min() {
-        return data.getLast();
+        return min.getFirst();
     }
 
     public int size() {
         return data.size();
     }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
 }
